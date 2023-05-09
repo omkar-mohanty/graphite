081b649caa5c238049a1a26eb4e2595db317b838
omkar-mohanty <franzohouser@gmail.com>
2023-05-09 21:32:15 +0530

    add note trait

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub trait Node {
+    
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub struct GraphDB {
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
