ac577d0885008ba01f6281344ec1f355dc70958d
omkar-mohanty <franzohouser@gmail.com>
2023-05-11 21:20:37 +0530

    Add node struct

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,7 +1,16 @@
-pub trait Node {
-    
+use std::marker::PhantomData;
+
+use serde::{Deserialize, Serialize};
+
+pub struct Node<'a, R: NodeData<'a>> {
+    _marker: &'a PhantomData<R>,
+    data: R,
 }
 
-pub struct GraphDB {
+pub trait NodeData<'a>: Serialize + Deserialize<'a> {}
 
+pub trait Edge<'a, V: NodeData<'a>> {
+    fn get_vertices(&'a self) -> (Node<V>, Node<V>);
 }
+
+pub struct GraphDB {}
