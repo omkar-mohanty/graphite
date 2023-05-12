da75cf3bbd38c076155e6ed481ab1d3c0528b14c
omkar-mohanty <franzohouser@gmail.com>
2023-05-12 21:50:38 +0530

    Add database struct

--- lib/src/lib.rs
+++ lib/src/lib.rs
@@ -1,15 +1,29 @@
-use serde::{Serialize, Deserialize};
+use std::collections::{BTreeMap, BTreeSet};
 use uuid::Uuid;
 
 pub struct Node {
     id: Uuid,
-    data: Box<dyn Serialize>
-    
+    data: Vec<u8>,
 }
 
 impl Node {
-    pub fn new(data: impl Serialize ) -> Self {
+    pub fn new(data: &[u8]) -> Self {
         let id = Uuid::new_v4();
-        Self { id, data }
+        Self {
+            id,
+            data: data.to_vec(),
+        }
+    }
+}
+
+pub struct Database {
+    nodes: BTreeMap<Uuid, Node>,
+}
+
+impl Database {
+    pub fn new() -> Self {
+        Database {
+            nodes: BTreeMap::new(),
+        }
     }
 }
